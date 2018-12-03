91ce0743b1c68d7e88d13307c6042e51c419f184
CharlesTheVictor <31681649+CharlesTheVictor@users.noreply.github.com>
2018-12-02 23:14:24 -0800

    POLARISUPDATE

--- POLARISWEBSITE/assets/script2.js
+++ POLARISWEBSITE/assets/script2.js
@@ -1,5 +1,9 @@
 $('document').ready(function(){
 
+	setTimeout(function(){
+		$(".loading").remove();
+	}, 6200);
+
   $(".CONCERNSBUTTON").click(function() {
      $(".CONCERNS").toggle();
 		 $(".PRIVATE").hide();
